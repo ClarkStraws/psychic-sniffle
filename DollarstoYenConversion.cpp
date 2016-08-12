6dd293d27a5915b3517e54fac3f7b22697c452fc
ClarkStraws <clarkstraws@gmail.com>
2016-08-11 21:53:11 -0400

    Add files via upload

---
+++ DollarstoYenConversion.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{int num;
+cout<<"Enter your monetary amount in US dollars"<<endl;
+cin>>num;
+cout<<"This is equivalent to"<<endl;
+cout<<num*102;
+cout<<" Japanese yen"<<endl;
+   return 0;
+}
